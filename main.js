883eea73650fdeeb8914413622ab49510f5c4ae2
Philipp Hellmayr <hellmayr.philipp@gmail.com>
2015-11-03 15:59:54 +0100

    show markers of near locations

--- main.js
+++ main.js
@@ -2,17 +2,22 @@ $(function() {
     $( "#tabs" ).tabs();
 });
 
+var map;
+var infowindow;
+var pos;
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    alert("init");
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 6
     });
-    var infoWindow = new google.maps.InfoWindow({map: map});
+    infoWindow = new google.maps.InfoWindow({map: map});
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
+            pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
@@ -20,6 +25,14 @@ function initMap() {
             infoWindow.setPosition(pos);
             infoWindow.setContent('You are here');
             map.setCenter(pos);
+
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: {lat: pos.lat, lng: pos.lng},
+                radius: 50000,
+                openNow: true
+            }, callback);
+
         }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
         });
@@ -27,6 +40,7 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -34,4 +48,28 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setContent(browserHasGeolocation ?
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
+}
+
+
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        alert(results.length);
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
 }
