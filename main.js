affe4986c99cf0918fd09dff6351c7393a0c903a
Philipp Hellmayr <hellmayr.philipp@gmail.com>
2015-11-04 20:46:20 +0100

    search position and display it on the map

--- main.js
+++ main.js
@@ -124,31 +124,32 @@ function initMap() {
         anchorPoint: new google.maps.Point(0, -29)
     });
     //
-    //autocomplete.addListener('place_changed', function() {
-    //    infowindow.close();
-    //    marker.setVisible(false);
-    //    var place = autocomplete.getPlace();
-    //    if (!place.geometry) {
-    //        window.alert("Autocomplete's returned place contains no geometry");
-    //        return;
-    //    }
+
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            return;
+        }
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+        }
+        marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+    });
     //
-    //    // If the place has a geometry, then present it on a map.
-    //    if (place.geometry.viewport) {
-    //        map.fitBounds(place.geometry.viewport);
-    //    } else {
-    //        map.setCenter(place.geometry.location);
-    //        map.setZoom(17);  // Why 17? Because it looks good.
-    //    }
-    //    marker.setIcon(/** @type {google.maps.Icon} */({
-    //        url: place.icon,
-    //        size: new google.maps.Size(71, 71),
-    //        origin: new google.maps.Point(0, 0),
-    //        anchor: new google.maps.Point(17, 34),
-    //        scaledSize: new google.maps.Size(35, 35)
-    //    }));
-    //    marker.setPosition(place.geometry.location);
-    //    marker.setVisible(true);
     //
     //    var address = '';
     //    if (place.address_components) {
